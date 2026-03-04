e3b74a51209ab3f6590214fbfa191032a0faa189
Aleksey Novikov <novikov82@gmail.com>
2026-03-04 11:29:58 +0300

    fix themes patches

--- convert.js
+++ convert.js
@@ -62,14 +62,14 @@ const THEME_CONFIG = [
       "--sjs2-color-bg-basic-primary-dim": "#f5f5f5",
       "--sjs2-color-fg-basic-primary-muted": "#000000bf",
       "--sjs2-color-utility-scrollbar": "#00000026",
-      "--sjs2-color-utility-overlay-screen": "color(srgb 0.800112 0.933371 0.933371 / 0.35)",
+      "--sjs2-color-utility-overlay": "color(srgb 0.800112 0.933371 0.933371 / 0.35)",
       "--sjs2-color-border-basic-secondary": "#dcdcdcff",
       "--sjs2-color-border-basic-secondary-for-tests-only": "#d4d4d4",
-      "--sjs2-color-utility-shadow-medium": "#004c441a",
+      "--sjs2-color-utility-shadow-floating-default": "#004c441a",
       "--sjs2-color-bg-basic-secondary-dim": "#eaeaeaff",
       "--sjs2-color-bg-basic-secondary": "#f4f4f4ff",
 	    "--sjs2-color-bg-accent-secondary-dim": "#19b39440",
-      "--sjs2-color-control-formbox-design-bg": "#f8f8f8ff",
+      "--sjs2-color-component-formbox-design-bg": "#f8f8f8ff",
       "--sjs2-color-utility-shadow-surface-default": "#00000040"
     },
     products: ["survey-creator"]
